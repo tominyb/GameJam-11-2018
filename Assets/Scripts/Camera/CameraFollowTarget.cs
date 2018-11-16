2f00b3c4dddd3af0cdc982c6cc54b1a889b7c74b
Tomi Nybäck <tominyb@gmail.com>
2018-11-16 20:33:41 +0200

    Add following camera

---
+++ Assets/Scripts/Camera/CameraFollowTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowTarget : MonoBehaviour
+{
+    [SerializeField] private float      m_speed;
+    [SerializeField] private Vector2    m_offset;
+    [SerializeField] private GameObject m_target;
+
+    private void LateUpdate()
+    {
+        if (m_target == null)
+            return;
+
+        var xy = Vector2.Lerp(
+            transform.position,
+            (Vector2)m_target.transform.position + m_offset,
+            m_speed * Time.deltaTime
+        );
+        transform.position = new Vector3(xy.x, xy.y, transform.position.z);
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        m_target = newTarget;
+    }
+}
