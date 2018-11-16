e810ac7b64ca43deca44bfff92c472e09535e383
Tomi Nybäck <tominyb@gmail.com>
2018-11-16 21:15:07 +0200

    Rename PlatformBehaviour to SurfaceBehaviour

---
+++ Assets/Scripts/Track/SurfaceBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SurfaceBehaviour : MonoBehaviour
+{
+    [System.Flags]
+    public enum Type
+    {
+        Accelerating,
+        Decelerating,
+        Magnetic,
+    }
+    
+    [SerializeField] float m_forceFactor;
+    [SerializeField] Type  m_type;
+
+    private void HandleContact(float step, Rigidbody2D other)
+    {
+
+    }
+}
