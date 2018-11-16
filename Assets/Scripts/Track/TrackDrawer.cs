94bc01e2002c638846df3fee70305a1a000dcad9
tominyb <tominyb@gmail.com>
2018-11-16 21:01:02 +0200

    Started work on track drawer.

---
+++ Assets/Scripts/Track/TrackDrawer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrackDrawer : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private List<Vector3> m_points = new List<Vector3>();
+
+    private float m_distanceTraversed = 0.0f;
+
+    private const float DistanceBetweenPoints = 0.5f;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_points.Clear();
+        m_points.Add(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        m_distanceTraversed += Vector3.Distance(m_points[m_points.Count - 1], eventData.position);
+
+        if (m_distanceTraversed >= DistanceBetweenPoints)
+        {
+            m_points.Add(eventData.position);
+            m_distanceTraversed = 0.0f;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        m_points.Add(eventData.position);
+        Draw();
+    }
+
+    public void Draw()
+    {
+        foreach (Vector3 point in m_points)
+        {
+            GameObject cube         = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Camera.main.ScreenToWorldPoint(point);
+        }
+    }
+}
