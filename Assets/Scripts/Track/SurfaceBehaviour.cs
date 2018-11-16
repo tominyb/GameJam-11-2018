b78407abba76ac6b84876bddfdef78342bfca081
Tomi Nybäck <tominyb@gmail.com>
2018-11-16 21:25:05 +0200

    Add surface acceleration

--- Assets/Scripts/Track/SurfaceBehaviour.cs
+++ Assets/Scripts/Track/SurfaceBehaviour.cs
@@ -2,19 +2,43 @@
 
 public class SurfaceBehaviour : MonoBehaviour
 {
-    [System.Flags]
     public enum Type
     {
         Accelerating,
-        Decelerating,
         Magnetic,
     }
-    
+
     [SerializeField] float m_forceFactor;
     [SerializeField] Type  m_type;
 
-    private void HandleContact(float step, Rigidbody2D other)
+    private void HandleContact(Rigidbody2D other)
+    {
+        switch (m_type)
+        {
+            case (Type.Accelerating):
+                Accelerate(other);
+                break;
+            case (Type.Magnetic):
+                Pull(other);
+                break;
+        }
+    }
+
+    private void Accelerate(Rigidbody2D other)
+    {
+        other.velocity *= m_forceFactor;
+    }
+
+    private void Pull(Rigidbody2D other)
     {
 
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!collision.rigidbody)
+            return;
+
+        HandleContact(collision.rigidbody);
     }
 }
