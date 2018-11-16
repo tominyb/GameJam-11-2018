3d0df19d5c55ae76cdf2a09d96864b991b847b9f
tominyb <tominyb@gmail.com>
2018-11-17 00:25:23 +0200

    Added bunch of different monster types.

---
+++ Assets/Scripts/Monsters/HorizontalMonster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMonster : Monster
+{
+    [SerializeField] private float m_patrolDistance;
+
+    private Vector3 m_startingPoint;
+    private Vector3 m_direction;
+
+    private void Awake()
+    {
+        m_startingPoint = transform.position;
+        int direction   = Random.Range(0,2);
+        m_direction     = direction == 0 ? Vector3.left : Vector3.right;
+    }
+
+    protected override void Move()
+    {
+        if (Vector3.Distance(m_startingPoint, transform.position) > m_patrolDistance)
+        {
+            m_direction = -m_direction;
+        }
+
+        transform.position += m_speed * m_direction * Time.fixedDeltaTime;
+    }
+}
