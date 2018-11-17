80dd46929f562515e65b7eade6108df3984c7cee
Aleksanteri Hirvonen <aleksanteri.hirvonen@gmail.com>
2018-11-17 13:45:05 +0200

    Start working on health display

---
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject m_icon;
+    [SerializeField] private int m_health;
+
+    private GameObject[] m_icons;
+
+    private void Start()
+    {
+        m_icons = new GameObject[m_health];
+        for (int i = 0; i < m_health; ++i)
+        {
+            m_icons[i] = Instantiate(m_icon, transform);
+        }
+    }
+}
