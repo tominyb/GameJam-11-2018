32d80b055669ce3f3cee9e745a35229beda562d0
Tomi Nybäck <tominyb@gmail.com>
2018-11-17 02:25:24 +0200

    Make camera movement smoother

---
+++ Assets/Scripts/Utils/BoundingBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BoundingBox
+{
+    [SerializeField] private Vector2 m_min;
+    [SerializeField] private Vector2 m_max;
+
+    public BoundingBox(Vector2 min, Vector2 max)
+    {
+        m_min = min;
+        m_max = max;
+    }
+
+    public Vector2 ClampInside(Vector2 v)
+    {
+        return new Vector2(
+            Mathf.Clamp(v.x, m_min.x, m_max.x),
+            Mathf.Clamp(v.y, m_min.y, m_max.y)
+        );
+    }
+}
